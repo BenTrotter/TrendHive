5196deef63c37f6a1a14e40fa2eb3bd17108f20e
BenTrotter <benjamin.trotter@yahoo.co.uk>
2024-08-13 21:52:22 +0100

    Git ignore and main.py

--- main.py
+++ main.py
@@ -1,1 +1,46 @@
-print("Hello")
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from datetime import datetime, timedelta
+from selenium.common.exceptions import TimeoutException
+import time
+import sys
+import re
+
+
+def open_page(driver, date):
+    url = get_url(date)
+    driver.get(url)
+
+def login(driver):
+    return
+
+def enter_text(driver, xpath, text):
+    wait = WebDriverWait(driver, 10)
+    input_element = wait.until(EC.visibility_of_element_located((By.XPATH, xpath)))
+    input_element.clear()
+    input_element.send_keys(text)
+
+def get_url(date):
+    url = ""
+    return url
+
+def wait_click(driver, xpath):
+    wait = WebDriverWait(driver, 10)
+    button = wait.until(EC.presence_of_element_located((By.XPATH, xpath)))
+    button.click()
+
+
+
+def main():
+    chrome_options = Options()
+    chrome_options.add_argument("--disable-popup-blocking")
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+
+if __name__ == "__main__":
+    main()
